9954732d5519f01c32cf7eed8697c48d5f0ebe7e
JPogodzinski <pogodzinskij@gmail.com>
2020-04-22 14:11:09 +0200

    Initial commit

---
+++ src/main/kotlin/com/nbapp/referee/RefereeApplication.kt
@@ -0,0 +1,11 @@
+package com.nbapp.referee
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RefereeApplication
+
+fun main(args: Array<String>) {
+    runApplication<RefereeApplication>(*args)
+}
