683978dd226a6925f3f2d9d84695f6e13f9c5bb9
lobby150 <rejwen10@interia.pl>
2020-06-10 14:28:51 +0200

    updated Game and GameService

--- src/main/kotlin/com/nbapp/referee/modules/game/GameService.kt
+++ src/main/kotlin/com/nbapp/referee/modules/game/GameService.kt
@@ -18,9 +18,9 @@ class GameService(private val gameRepository: GameRepository, private val teamRe
         val hostResult:Int=text.getValue("hostResult").toInt()
         val guestResult:Int=text.getValue("guestResult").toInt()
         val game:Game=gameRepository.getOne(id)
-        val hostId:Int=game.hostId
-        val guestId:Int=game.guestId
-        val host: Team = teamRepository.getOne(hostId)
+        val hostId:Int=game.host.teamId
+        val guestId:Int=game.guest.teamId
+        val host:Team= teamRepository.getOne(hostId)
         val guest:Team=teamRepository.getOne(guestId)
         game.updateHostResult(hostResult)
         game.updateGuestResult(guestResult)
